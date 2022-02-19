407541562e5f1899f4f1b4dfe72cc8b9bcd30cf4
Arastuncode <92625300+Arastuncode@users.noreply.github.com>
2022-02-20 01:18:19 +0400

    Front-end full

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,10 +1,48 @@
+$(function(){
 
-$(document).ready(function () {
-  $("h6").hover(function () {
-   $(this).addClass("active")
-  });  
-  $("h6").mouseout(function () {
-      $(this).removeClass("active")
-     });  
-});
+    console.log("!");
+    
+    var clock = $('.clock').FlipClock({
+        clockFace: 'DailyCounter',
+        autoStart: false,
+        callbacks: {
+          stop: function() {
+            $('.message').html('The clock has stopped!')
+          }
+        }
+    }); 
+    clock.setTime(220880);
+
+    
+
+// countdown mode
+clock.setCountdown(true);
+
+// start the clock
+clock.start();
+
+})
+    
+// set time
+
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+})
+
+
+
 
